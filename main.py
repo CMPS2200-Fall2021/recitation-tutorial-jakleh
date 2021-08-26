a6f7b218c434c675b2e988d1f2dd31fd6bff7fe5
jakleh <jslehner16@gmail.com>
2021-08-26 08:49:09 -0500

    Modify main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def sum_of_squares(a):
-	pass
+	sum = 0
+	for i in a:
+		sum += i**2
+	return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
 
-#My first comment
+print(test_one())	#Prints "None"
+
+#Test w/ negative nums...
+def test_two():
+	assert sum_of_squares([-1, -2, -3]) == 14
+
+print(test_two())	#Prints 
